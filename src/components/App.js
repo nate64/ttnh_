4dbab0cfbf57cbdba821c66e80c31e76e18e8be6
Nate <nathanestes@gmail.com>
2018-12-05 15:04:48 -0700

    initial, plus some more

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+import Navbar from './navbar'
+
+import { Button } from 'semantic-ui-react'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default App;
