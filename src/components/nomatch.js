b1788d05f789b00be7846d7b39372c028ff08ddd
Nate <nathanestes@gmail.com>
2018-12-10 15:43:37 -0700

    just img file mgmt

--- src/components/nomatch.js
+++ src/components/nomatch.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const NoMatch = () => (
-  <h2>404</h2>
+  <div>
+    <h2>Page Not Found.</h2>
+    <h2>You May Have Been Using Bad Herbs.</h2>
+    <h2>Click Here To Go Home</h2>
+  </div>  
 )
 
 export default NoMatch;
