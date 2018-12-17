c0ef94640425c9dc1f1a77e0c488535fd25183da
Nate <nathanestes@gmail.com>
2018-12-17 16:37:01 -0700

    styling is going to kill me

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,19 +2,45 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { Container, Grid } from 'semantic-ui-react'
+
 import Barn from '../images/barn.jpg'
+import Map from '../images/maps_screenshot.png'
 
 const contactgridcentered = () => (
-  <Grid centered columns={4}>
+  <Grid centered columns={2}>
+    
     <Grid.Column>
-      <h3>contact us info</h3>
+      <LeftContainer>
+        <h3>contact us info</h3>
+      </LeftContainer>
     </Grid.Column>
+    
     <Grid.Column>
-      <h3>Google Map here</h3>
+      <RightContainer>
+        <h3>Google Map here</h3>
+      </RightContainer>
     </Grid.Column>
     
   </Grid>
 )
+
+const LeftContainer = styled.div`
+  padding-top: 500px;
+  background-image: url(${Barn});
+  background-size: contain;
+  background-repeat: no-repeat;
+  
+  // background: #fffdd0;
+  // background-position: center;
+`
+
+const RightContainer = styled.div`
+  padding-top: 500px;
+  background-image: url(${Map});
+  background-size: contain;
+  background-repeat: no-repeat;
+
+`
 
 export default contactgridcentered
 
@@ -42,16 +68,7 @@ export default contactgridcentered
 // }
 
 
-// const PageContainer = styled.div`
-//   background: #fffdd0;
-  
-//   // background-image: url(${Barn});
-//   // background-position: center;
-//   // background-size: contain;
-//   // background-repeat: no-repeat;
 
-
-// `
 
 
 // export default Products
