1968c4794490c8debfc4c7091a4fe20a7b8f94e4
Nate <nathanestes@gmail.com>
2018-12-20 21:24:41 -0700

    home page formatting and styling

--- src/components/home.js
+++ src/components/home.js
@@ -6,51 +6,57 @@ import { Link } from 'react-router-dom'
 import Toshastore from '../images/Tosha_store.jpg'
 import Sign from '../images/sign.jpg'
 import smallLogo from '../images/ttnh_logo_nobackground.png'
+import Threeherbs from '../images/3_herbs.jpg'
 
 
 const homegrid = () => (
   <div>
 
     <Grid divided='vertically'>
-      <Grid.Row columns={12}>
+      <Grid.Row columns={2}>
 
-      <Grid.Column>
-        <Container>
-        <Link to='/'>
-          <Logo src={smallLogo} />
-        </Link> 
-        </Container>
-      </Grid.Column>
+        <Grid.Column>
+          <Container>
+            <Logo src={smallLogo} />
+          </Container>
+        </Grid.Column>
 
-
-      <Grid.Column>
-
-      </Grid.Column>
+        <Grid.Column>
+          <Container fluid>
+            <Segment basic>
+              <h1>Welcome to Wellness</h1>
+            </Segment>
+          </Container>
+        </Grid.Column>
 
       </Grid.Row>
     </Grid>
 
+    
+      <Homehero fluid>
+        {/* text and a button here? */}
+      </Homehero>
 
-    <Container>
-
-    </Container>
+    
   </div>
 )
 
 
 
 
-
 const Homehero = styled.div`
-  background-image: url(${Sign});  
-  background-size: cover;
+  background-image: url(${Threeherbs});  
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100%;
+  padding-top: 850px;
 `
 
 const Logo = styled.img`
   height: 501px;
   width: 397px;
   margin-left: 10px;
-  margin-right: 25px;
+  margin-right: 10px;
   margin-top: 10px;
   margin-bottom: 10px;
 `
